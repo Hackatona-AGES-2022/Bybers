ec8f77b5a0fd275e215caf94887863004055ee9c
joaolaureano <laureano.pedrojoao@gmail.com>
2022-05-28 08:09:53 -0300

    Routine page

--- Front/src/Pages/Test/index.tsx
+++ Front/src/Pages/Test/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
 
-function Test() {
+export function Test() {
   return (
 ""
   );
 
-export default Test;
+  }
---
+++ Front/src/Pages/routine-definition/style.ts
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Container = styled("div")`
+  display: flex;
+  align-items:center;
+  flex-direction: column;
+`;
+
+export const GapContainer = styled("div")`
+  display: flex;
+  align-items:center;
+  flex-direction: column;
+  gap: 10px
+`;
+
+export const Bottom = styled("div")`
+position:absolute;
+bottom: 0;
+margin-bottom:15px;
+`;
+
