41d30c6a5e67a043049120e429be945a095d7e23
joaolaureano <laureano.pedrojoao@gmail.com>
2022-05-28 05:31:06 -0300

    Final Config

---
+++ Front/src/Pages/Test/index.tsx
@@ -0,0 +1,14 @@
+import { FormEvent, useState } from 'react';
+import { Task } from '../../Component/TaskComponent';
+
+
+
+export function Test() {
+
+  return (
+      <>
+    <Task color='A0E3E25E' label='Teste' time='3 horas' />
+      </>
+    
+  );
+}
