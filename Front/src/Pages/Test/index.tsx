6a6b0c1076fd2c94517dcd40dcc75b3e3f30b97b
joaolaureano <laureano.pedrojoao@gmail.com>
2022-05-28 07:37:25 -0300

    Fixed conflic

--- Front/src/Pages/Test/index.tsx
+++ Front/src/Pages/Test/index.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import logo from './logo.svg';
-import SplitScreen from '../../Component/Test';
 
 function Test() {
   return (
-<SplitScreen></SplitScreen>
+""
   );
 }
 
