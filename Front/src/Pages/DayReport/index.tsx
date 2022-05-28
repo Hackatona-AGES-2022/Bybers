61cbc1c5ca1e077eccead4f093a6c4fad629c0ad
VarBoff <vinirechboff@gmail.com>
2022-05-28 18:53:51 -0300

    FINALEIRA

--- Front/src/Pages/DayReport/index.tsx
+++ Front/src/Pages/DayReport/index.tsx
@@ -6,48 +6,53 @@ import Subtitle from "../../Component/Subtitle";
 import Task from "../../Component/TaskComponent";
 import Title from "../../Component/Title";
 
+import { useNavigate } from "react-router-dom";
 import WeekDayComponentButton from "../../Component/WeekDayComponent";
 import TaskInput from "../../Component/TaskInfoInputComponent/TaskInputComponent";
 import {
-  Bottom,
-  Container,
-  GapContainer,
-  TextAlign,
-  WeekDayContainer,
-  Image,
-  ContainerSlider,
-  p,
+	Bottom,
+	Container,
+	GapContainer,
+	TextAlign,
+	WeekDayContainer,
+	Image,
+	ContainerSlider,
+	p,
 } from "./style";
 import emoticons from "../../Assets/imgs/emoticon.svg";
 function DayReport() {
-  const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+	let navigate = useNavigate();
+	const { enqueueSnackbar, closeSnackbar } = useSnackbar();
 
-  return (
-    <>
-      <Header />
-      <Container>
-        <Title title="Muito bem, Vinícius!" />
+	return (
+		<>
+			<Header />
+			<Container>
+				<Title title="Muito bem, Vinícius!" />
 
-        <TextAlign>
-          <Subtitle title="Você realizou 8 horas de atividades" />
-        </TextAlign>
+				<TextAlign>
+					<Subtitle title="Você realizou 8 horas de atividades" />
+				</TextAlign>
 
-        <TextAlign>
-          <Subtitle title="Você descansou 2 horas hoje! Muito bem. Descanso é essencial" />
-        </TextAlign>
-        <GapContainer></GapContainer>
-        <h3>Como foi seu dia?</h3>
-        <Image src={emoticons}></Image>
-        <ContainerSlider>
-          <SliderComponent />
-        </ContainerSlider>
+				<TextAlign>
+					<Subtitle title="Você descansou 2 horas hoje! Muito bem. Descanso é essencial" />
+				</TextAlign>
+				<GapContainer></GapContainer>
+				<h3>Como foi seu dia?</h3>
+				<Image src={emoticons}></Image>
+				<ContainerSlider>
+					<SliderComponent />
+				</ContainerSlider>
 
-        <TaskInput title="O que aconteceu de importante hoje?" />
-        <Bottom>
-          <Button text="Salvar relatório" onClick={null}></Button>
-        </Bottom>
-      </Container>
-    </>
-  );
+				<TaskInput title="O que aconteceu de importante hoje?" />
+				<Bottom>
+					<Button
+						text="Salvar relatório"
+						onClick={() => navigate(-1)}
+					></Button>
+				</Bottom>
+			</Container>
+		</>
+	);
 }
 export default DayReport;
