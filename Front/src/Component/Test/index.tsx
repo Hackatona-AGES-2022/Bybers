68b64026e9c64ecb6fedcfae474886d8556cee06
joaolaureano <laureano.pedrojoao@gmail.com>
2022-05-28 06:16:33 -0300

    Done example

---
+++ Front/src/Component/Test/index.tsx
@@ -0,0 +1,17 @@
+import React, { FC } from "react";
+import { Title } from "./style";
+
+interface Props {
+  
+}
+const Example: React.FC<Props> = () => {
+
+
+  return (
+    <Title>
+      a
+    </Title>
+  );
+};
+
+export default Example;
---
+++ Front/src/Pages/Test/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import logo from './logo.svg';
+import SplitScreen from '../../Component/Test';
+
+function Test() {
+  return (
+<SplitScreen></SplitScreen>
+  );
+}
+
+export default Test;
