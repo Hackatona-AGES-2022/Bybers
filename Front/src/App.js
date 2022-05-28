d2775291719b5606e3000d4cdd47e663d2ebe468
Vinícius André Rech Boff <vinicius.boff@weloze.com.br>
2022-05-28 03:58:47 -0300

    Configuração inicial das rotas para as telas

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import history from "./history";
+import "./App.css";
+
+/*
+  CRIAR ROTA:
+  <Route exatc path="CAMINHO" element={<COMPONENTE />} />
+*/
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter history={history}>
+			<Routes>
+				<Route exatc path="" element={<div />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
