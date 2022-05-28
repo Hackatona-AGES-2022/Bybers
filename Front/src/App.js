e58c5f9ea8ba3c28f89fd7a7843a8fe2e68a2553
VarBoff <vinirechboff@gmail.com>
2022-05-28 06:49:32 -0300

    corrigindo erros node-modules

--- Front/src/App.js
+++ Front/src/App.js
@@ -1,17 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Test from "./Pages/Test";
-import history from './history'
+import history from "./history";
 function App(props) {
-  /* Adicionar uma rota para um componente, nao esqueca de importa o componente
+	/* Adicionar uma rota para um componente, nao esqueca de importa o componente
      <Route exatc path="CAMINHO" element={<COMPONENTE/>} />
     */
-  return (
-    <BrowserRouter history={history}>
-          <Routes>
-            <Route exatc path="/" element={<Test />} />
-          </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter history={history}>
+			<Routes>
+				<Route exatc path="/" element={<Test />} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
